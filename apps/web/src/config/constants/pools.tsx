3c2c69129ac206e0330b3f6e15cebd5b57ac42a4
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2024-05-12 11:01:20 +0200

    Update pools.tsx

--- apps/web/src/config/constants/pools.tsx
+++ apps/web/src/config/constants/pools.tsx
@@ -2719,19 +2719,6 @@ const finishedPools = [
     harvest: true,
     tokenPerBlock: '115000000',
     isFinished: true,
-   },
-  {
-    sousId: 203,
-    stakingToken: bscTokens.cake,
-    earningToken: bscTokens.bullstar,
-    contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0xD1B6A1f961526D7866fBcD3EB80653ca837a11B5',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '0.495',
-    isFinished: false,
   }
 ].map((p) => ({
   ...p,
