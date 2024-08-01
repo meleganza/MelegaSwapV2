ab70e8f6b54c2a12dbe6f7a4043c20b782f9ec8d
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2024-08-01 09:59:29 +0400

    Update pools.tsx

--- apps/web/src/config/constants/pools.tsx
+++ apps/web/src/config/constants/pools.tsx
@@ -1799,7 +1799,7 @@ export const livePools: Pool.SerializedPoolConfig<SerializedWrappedToken>[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '1',
+    tokenPerBlock: '2',
     isFinished: false,
    },
   {
