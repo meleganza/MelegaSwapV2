08830683b8c48373e9de96cfa14aba0d07d58eb7
MelegaSwap <111182693+meleganza@users.noreply.github.com>
2025-08-03 09:13:44 +0200

    Update 137.ts

--- packages/tokens/src/137.ts
+++ packages/tokens/src/137.ts
@@ -169,5 +169,12 @@ export const polygonTokens = {
     18,
     'SAUDI',
     'SAUDI'
+  ),
+  saudi: new ERC20Token(
+    ChainId.POLYGON,
+    '0x17A13A4ABA33fB0ca76CeC97580CDadd9df18738',
+    18,
+    'CSN',
+    'Crypto Smart Network'
   )
 }
